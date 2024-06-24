43fca80ffe003c2d8182dfaaaa4da966db43a5cc
Ibuki Yamada <135494855+ikkunN700S@users.noreply.github.com>
2024-06-24 16:59:20 +0900

    index.htmlのリスト読み込み対応

--- js/common.js
+++ js/common.js
@@ -1,3 +1,26 @@
+// ヘッダ展開メニューのスクリプト
+window.addEventListener('load', function () {
+    var $button = document.querySelector('.toggle-menu-button');
+    var $menu = document.querySelector('.header-site-menu');
+
+    $button.addEventListener('click', function () {
+        if ($menu.classList.contains('is-show')) {
+            $menu.classList.remove('is-show');
+        }
+        else {
+            $menu.classList.add('is-show');
+        }
+        });
+    });
+    
+    $(function() {
+        $(window).scroll(function() {
+            $("nav.floating").stop().animate (
+                {"top": $(window).scrollTop() + 100}, 500
+            );
+        });
+    });
+
 /*id=
     remain-time 全課題残り時間
 
