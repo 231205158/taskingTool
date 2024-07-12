6b018c27b6cd372496552bc9111f1ded3376479d
Koki Hiromura <231205158@ccmailg.meijo-u.ac.jp>
2024-07-12 14:17:02 +0900

    再びedit

--- js/edit.js
+++ js/edit.js
@@ -9,12 +9,10 @@
 
 const loadLocalStorage = () => {
     var index = JSON.parse(localStorage.getItem('editIndex')) || -1;
-    var editTask;
-    editTask = (JSON.parse(localStorage.getItem('preTasks'))[index] ||
-        [{text:'タスク名', limit:'yyyy/mm/dd', expectTime:0, didTimes:[0], editDates:['']}]);
+    var editTask = (JSON.parse(localStorage.getItem('preTasks'))[index] || [{text:'タスク名', limit:'', expectTime:0, didTimes:[0], editDates:['']}]);
 
     document.getElementById('task-text').value = editTask.text;
-    document.getElementById('task-time-did').value = editTask.didTimes[exListPre.didTimes.length -1];
+    document.getElementById('task-time-did').value = editTask.didTimes[editTask.didTimes.length -1];
     document.getElementById('task-time-expect').value = editTask.expectTime;
     document.getElementById('task-limit').value = editTask.limit;
 };
@@ -26,18 +24,19 @@ document.addEventListener('DOMContentLoaded', () => {
         var taskList = JSON.parse(localStorage.getItem('preTasks')) || [];
         var taskElements = {text:'', limit:'', expectTime:0, didTimes:[0], editDates:['']};
         var index = JSON.parse(localStorage.getItem('editIndex')) || -1;
+
         taskElements.text = document.getElementById('task-text').value;
         taskElements.limit = document.getElementById('task-limit').value;
         taskElements.expectTime = document.getElementById('task-time-expect').value;
         taskElements.didTimes.push(document.getElementById('task-time-did').value);
     
         var today = new Date();
-        var dayStr = today.getFullYear() + '/' + today.getMonth() + '/' + today.getDate()
+        var dayStr = today.getFullYear() + '/' + String(today.getMonth() + 1) + '/' + String(today.getDate());
         taskElements.editDates.push(dayStr);
         if(index != -1){
-            taskList.splice(index, 1)
-        }
-        taskList.push(taskElements)
+            taskList.splice(index, 1);
+        };
+        taskList.push(taskElements);
         localStorage.setItem('preTasks', JSON.stringify(taskList));
         window.location.href = 'taskprefinish.html';
     });
